ce207b844abf96b7ff95d51aff3a3a8e76ef7c67
litingyang <562836244@qq.com>
2025-03-28 12:01:19 +0800

    update

---
+++ 160.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode1(ListNode *headA, ListNode *headB) 
+    {
+        std::vector<ListNode*> la;
+        std::vector<ListNode*> lb;
+        
+        for (auto ptr = headA; ptr != nullptr; ptr = ptr->next) {
+            la.push_back(ptr);
+        }
+        for (auto ptr = headB; ptr != nullptr; ptr = ptr->next) {
+            lb.push_back(ptr);
+        }
+
+        std::reverse(la.begin(), la.end());
+        std::reverse(lb.begin(), lb.end());
+
+        size_t i = 0;
+        for (; i < la.size() && i < lb.size(); i++) {
+            if (la[i] != lb[i]) {
+                if (i == 0) {
+                    return nullptr;
+                } else {
+                    return la[i-1];
+                }
+            }
+        }
+
+        if (i == la.size()) {
+            return la.back();
+        } else {
+            return lb.back();
+        }
+    }
+
+    ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB) 
+    {
+        std::unordered_set<ListNode*> set_a;
+        for (auto ptr = headA; ptr != nullptr; ptr = ptr->next) {
+            set_a.insert(ptr);
+        }
+
+        for (auto ptr = headB; ptr != nullptr; ptr = ptr->next) {
+            if (set_a.find(ptr) != set_a.end()) {
+                return ptr;
+            }
+        }
+        return nullptr;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+    }
+};
---
+++ 234.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {
+    public:
+        std::generator<ListNode*> getReverseNodes(ListNode* cur)
+        {
+            if (!cur)
+                co_return;
+            if (!cur->next) {
+                co_yield std::ranges::elements_of(getReverseNodes(cur->next));
+            }
+            co_yield cur;
+        }
+
+        bool isPalindrome1(ListNode* head) 
+        {
+            // 原理和递归解法一样但是比递归解法好理解
+            // 这里用到了generator来产生逆序的节点
+            auto gen = getReverseNodes(head);
+            auto ptr = head;
+            for (ListNode* tail : gen) {
+                if (ptr->val != tail->val)
+                   return false;
+                ptr = ptr->next;
+            }
+            return true;
+        }
+
+        void isPalindrome2Impl(ListNode* cur, ListNode *& front, bool &short_cuts)
+        {
+            if (!cur)
+                return;
+            if (short_cuts)
+                return;
+            isPalindrome2Impl(cur->next, front, short_cuts);
+            if (cur->val != front->val) {
+                short_cuts = true;
+                return;
+            }
+            front = front->next;
+        }
+
+        bool isPalindrome2(ListNode* head) 
+        {
+            // 递归解法(有两个状态量，不好理解)
+            ListNode* front = head;
+            bool short_cuts = false;
+            isPalindrome2Impl(head, front, short_cuts);
+            return front == nullptr;
+        }
+
+        bool isPalindrome3(ListNode* head)
+        {
+            // list转vector
+            std::vector<int> st;
+            for (auto ptr = head; ptr != nullptr; ptr = ptr->next) {
+                st.push_back(ptr->val);
+            }
+            return std::equal(st.cbegin(), st.cbegin() + st.size() / 2, st.crbegin());
+        }
+
+        bool isPalindrome(ListNode* head) {
+        }
+    };
